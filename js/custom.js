a0b04ed8adfe95f6b55d84c2222cf0056a8d94d0
swagata-km <swagatakreativetech@gmail.com>
2023-04-13 17:12:45 +0530

    add counter section js & magnific popup js

--- js/custom.js
+++ js/custom.js
@@ -19,27 +19,73 @@ $('#custom-owl').owlCarousel({
 });
 
 // ************************************************* //
-// * +++++++++++++ 02. Header sticky & navbar ++++++++++++++ * //
+// * +++++++++ 02. Header sticky & navbar ++++++++++ * //
 // ************************************************* //
 
-    $(window).on('scroll', function() {
-       if($(this).scrollTop() > 300 ) {
-       $(".header").addClass("fixed-top");
-       $(".header__top").slideUp();
-      } else {
-       $(".header").removeClass("fixed-top");
-       $(".header__top").slideDown();
-      }
-    });
-    $(".navbar-toggler").on('click', function()  {
-      $(this).toggleClass("collapsed")
-    })
+$(window).on('scroll', function() {
+    if($(this).scrollTop() > 300 ) {
+    $(".header").addClass("fixed-top");
+    $(".header__top").slideUp();
+  } else {
+    $(".header").removeClass("fixed-top");
+    $(".header__top").slideDown();
+  }
+});
+$(".navbar-toggler").on('click', function()  {
+  $(this).toggleClass("collapsed")
+})
 
-    // dropdown on hover
-    $(".dropdown").hover( function() {
-      if ($(window).width() > 991) {
-        $(this).find(".dropdown-menu").toggleClass("show")
-      }
-    })
+// dropdown on hover
+$(".dropdown").hover( function() {
+  if ($(window).width() > 991) {
+    $(this).find(".dropdown-menu").toggleClass("show")
+  }
+})
 
+// ************************************************* //
+// * ++++++++++ 03. Counter Section +++++++++++ * //
+// ************************************************* //
+jQuery('.count-number').each(function () {
+  jQuery(this).prop('Counter',0).animate({
+      Counter: jQuery(this).text()
+  }, {
+    
+    //chnage count up speed here
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          jQuery(this).text(Math.ceil(now));
+      }
+  });
+});
+
+// ************************************************* //
+// * ++++++++++ 04. Magnific Popup +++++++++++ * //
+// ************************************************* //   
+
+$('.popup-youtube').magnificPopup({
+  type: 'iframe',
+  iframe: {
+    patterns: {
+      youtube: {
+        index: 'youtube.com/',
+        id: function(url) {
+          var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+          if (!m || !m[1]) return null;
+          return m[1];
+        },
+        src: '//www.youtube.com/embed/%id%?autoplay=1'
+      },
+      vimeo: {
+        index: 'vimeo.com/',
+        id: function(url) {
+          var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+          if (!m || !m[5]) return null;
+          return m[5];
+        },
+        src: '//player.vimeo.com/video/%id%?autoplay=1'
+      }
+    }
+  }
+});
 
